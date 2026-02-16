c7f086ab08b376b89625a73111cb163bfda9778d
ReimuHak200 <rei.muhak200@outlook.com>
2026-02-16 17:41:31 -0600

    feat(client): Init Gensokyo Engine Client (React+Three.js)

---
+++ client/src/App.tsx
@@ -0,0 +1,53 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Text } from '@react-three/drei'
+import * as THREE from 'three'
+
+function Box(props: any) {
+  // This reference gives us direct access to the mesh
+  const ref = useRef<THREE.Mesh>(null!)
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+function Ground() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+      <planeGeometry args={[100, 100]} />
+      <meshStandardMaterial color="#333" />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 2, 5] }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <Ground />
+      <OrbitControls />
+      <Text position={[0, 2, 0]} fontSize={0.5} color="white">
+        Gensokyo Engine v0.1
+      </Text>
+    </Canvas>
+  )
+}
